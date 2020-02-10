f04642dc60986ebecc01ae3d30116f55b7630c47
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-02-10 23:16:18 +0700

    Test auto type conversion

---
+++ 05_auto_type_conv.js
@@ -0,0 +1,15 @@
+console.log("8 * null = ", 8 * null);
+
+console.log("\"5\" - 1 = ", "5" - 1);
+
+console.log("\"5\" + 1 = ", "5" + 1);
+
+console.log("\"five\"*2 = ", "five"*2);
+
+console.log("five = " + 2 + 3)
+
+console.log("five = " + (2 + 3))
+
+console.log("false == 0 = ", false == 0);
+
+console.log("false === 0 = ", false === 0);
