003bb74120ec7513a7a22ca41c4660a4c3d4d8ba
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-02-24 22:28:08 +0700

    Test constant

--- ex_require/MAIN.js
+++ ex_require/MAIN.js
@@ -1,4 +1,8 @@
 const amanda_calculate = require("./amanda.js");
 const tyler_calculate = require("./tyler.js");
+const A = require("./data.js")
+
+console.log(amanda_calculate(2,1,3));
 console.log(tyler_calculate(2));
 
+console.log("Constants: ", A.split(" "));
